938339760b91916ae332b71aa8f64860b3778b73
SakaevaDiana <sakaevadiana2007@gmail.com>
2025-03-21 09:02:43 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -5,7 +5,9 @@ int main()
     float Celsius;
     printf("Введите температуру в градусах Цельсия ");
     scanf("%f", &Celsius);
+    
     float Fahrenheit = (Celsius * 9/5) + 32;
+    
     printf("C = %.0f ; F = %.2f", Celsius, Fahrenheit);
     return 0;
 }
