73f6db69ec4aaa78ff6c97b6949b31ce0b104de2
SakaevaDiana <sakaevadiana2007@gmail.com>
2025-03-21 09:29:43 +0300

    Update 3.c

--- 3.c
+++ 3.c
@@ -5,7 +5,12 @@ int main() {
     printf("Введите три числа: ");
     scanf("%d %d %d", &a, &b, &c);
     
-    int product_sign = (a == 0 || b == 0 || c == 0) * 1 + (a > 0) * (b > 0) * (c > 0) * 2 - 1;
+    // если хотя бы одно число 0, значение = 1
+    int zero = (a == 0 || b == 0 || c == 0);
+    
+    int number_sign = (a > 0) * (b > 0) * (c > 0);
+    
+    int product_sign = zero * 1 + number_sign * 2 - 1;
     
     printf("%d\n", product_sign);
     return 0;
