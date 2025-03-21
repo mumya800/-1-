8805600816a2bb8b0c716ea8f73299b204294c89
SakaevaDiana <sakaevadiana2007@gmail.com>
2025-03-21 09:02:07 +0300

    Update and rename 4.ess to 4.c

--- 4.c
+++ 4.c
@@ -5,7 +5,7 @@ int main() {
     printf("Введите число: ");
     scanf("%d", &n);
     
-    int is_power_of_two = (n > 0) && ((n & (n - 1)) == 0);
+    int is_power_of_two = (n > 0) & !(n & (n - 1));
     
     printf("%d\n", is_power_of_two);
     return 0;
