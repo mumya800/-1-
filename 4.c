cf71121035a582c9b81eff20ced9be43d610487a
SakaevaDiana <sakaevadiana2007@gmail.com>
2025-03-24 12:39:35 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -4,9 +4,16 @@ int main() {
     int n;
     printf("Введите число: ");
     scanf("%d", &n);
+
+    // Проверка положительное ли n число
+    int plus = (n > 0);
     
-    int is_power_of_two = (n > 0) & !(n & (n - 1));
+    // Проверка того, что побитовая операция AND между n и n - 1 равна нулю
+    int is_power_of_two = (!(n & (n - 1)));
     
-    printf("%d\n", is_power_of_two);
+    int is_power_of_two_result = plus * is_power_of_two;
+    
+    // Для степени двойки вывод 1. Числа, не подходящие под условие, вывод - 0
+    printf("%d\n", is_power_of_two_result);
     return 0;
 }
